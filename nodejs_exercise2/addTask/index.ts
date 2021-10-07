eb95aa08c07b5c3c51088867f30f76244f133097
Vladimir <vlzakar@gmail.com>
2021-10-07 13:55:09 +0200

    Added Function app exercise with Nodejs and Typescript

---
+++ nodejs_exercise2/addTask/index.ts
@@ -0,0 +1,26 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.res = {
+        status: 400
+    };
+
+    if (req.body?.description) {
+        const task = {
+            id: new Date().toISOString() + Math.random().toString().substr(2,8),
+            description: req?.body?.description,
+            dueDate: req?.body?.dueDate,
+        }
+        context.bindings.newTask = JSON.stringify(task);
+        context.res = {
+            status: 201,
+            headers: {
+              Location: "/api/v1/tasks"
+            },
+            body: task
+        };
+    }
+
+};
+
+export default httpTrigger;
